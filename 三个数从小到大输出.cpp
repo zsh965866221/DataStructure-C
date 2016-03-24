2b4b118c4404028a70d13822162a004d628c01e4
zsh_o <965866221@qq.com>
2016-03-24 21:47:01 +0800

    Update 三个数从小到大输出.cpp

--- 三个数从小到大输出.cpp
+++ 三个数从小到大输出.cpp
@@ -1,5 +1,17 @@
 #include <stdio.h>
+
+void _swap(int &a,int &b)
+{
+    a=a^b;
+    b=a^b;
+    a=a^b;
+}
 int main()
 {
-
+    int a,b,c;
+    scanf("%d%d%d",&a,&b,&c);
+    if(a>b)     _swap(a,b);
+    if(a>c)     _swap(a,c);
+    if(b>c)     _swap(b,c);
+    printf("%d,%d,%d",a,b,c);
 }
